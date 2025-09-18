cd5f82a39c529742277d7e33d7b403ed90c74483
Maksim Khandeshin <maksimhandeshin@gmail.com>
2025-09-18 23:33:37 +0300

    make hw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import user from "./data/user.json";
+import { Profile } from "./Profile/Profile";
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import user from "../data/user.json";
+export const Profile = () => {
+  console.log(user.username);
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src={user.avatar}
+          alt={user.tag}
+          class="avatar"
+          width={150}
+          height={150}
+        />
+        <p class="name">{user.username}</p>
+        <p class="tag">{user.tag}</p>
+        <p class="location">{user.location}</p>
+      </div>
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
