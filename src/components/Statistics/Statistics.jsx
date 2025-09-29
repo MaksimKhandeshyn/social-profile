776558dfa02453b1a1e840cdcfded8645e93e277
Maksim Khandeshin <maksimhandeshin@gmail.com>
2025-09-29 09:21:41 +0300

    make hw

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,12 @@
 import module from "./Statistis.module.css";
 export const Statistics = ({ title, stats }) => {
+  console.log(module);
   return (
     <section className="statistics">
-      {title && <h2 className="static__title">{title}</h2>}
+      {title && <h2 className={module.staticTitle}>{title}</h2>}
       <ul className="stat-list">
         {stats.map(({ id, label, percentage }) => (
-          <li className="item" key={id}>
+          <li className={module.item} key={id}>
             <span className="label">{label}</span>
             <span className="percentage">{percentage}%</span>
           </li>
